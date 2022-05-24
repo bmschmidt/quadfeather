c2c32cb884c17402e01ca9fef15dbb4e45fa8b84
Ben Schmidt <bmschmidt@gmail.com>
2022-05-24 09:13:37 -0400

    Fix calculation of bounds when passed object is feather/parquet without defined bounds

--- quadfeather/ingester.py
+++ quadfeather/ingester.py
@@ -3,12 +3,13 @@ from pyarrow import parquet as pq, feather, json as pajson, csv
 from pathlib import Path
 
 class Ingester:
-  def __init__(self, files : list[Path], batch_size : int = 1024 * 1024 * 1024):
+  def __init__(self, files : list[Path], batch_size : int = 1024 * 1024 * 1024, columns = None):
     # Allow iteration over a st
     # queue_size: maximum bytes in an insert chunk.
 
     self.files = files
     self.queue = []
+    self.columns = columns
     self.batch_size = batch_size
 
   def __iter__(self):
@@ -18,7 +19,7 @@ class Ingester:
       queue_length = queue_length + batch.nbytes
       if queue_length > self.batch_size:
         tb = pa.Table.from_batches(batches=self.queue)
-        yield tb.combine_chunks().batches[0]
+        yield tb.combine_chunks()
         self.queue = []
         queue_length = 0
     if len(self.queue) > 0:
@@ -29,7 +30,7 @@ class ArrowIngester(Ingester):
 
   def batches(self):
     for file in self.files:
-      feath = feather.read_table(file)
+      feath = feather.read_table(file, columns=self.columns)
       for batch in feath.to_batches():
         yield batch 
 
@@ -37,7 +38,7 @@ class ParquetIngester(Ingester):
   def batches(self):
     for f in self.files:
       f = pq.ParquetFile(f)
-      for batch in f.iter_batches():
+      for batch in f.iter_batches(columns=self.columns):
         yield batch
 
 def get_ingester(files : list[Path]) -> Ingester:
